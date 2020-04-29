fbccded1ee331ada1b0a0cc87eba55af424dacf9
Gerald <gerald.lau.95@gmail.com>
2020-04-30 01:54:08 +0800

    vue-flip working

---
+++ src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <vue-flip
+    :active-click="true"
+    height="384px"
+    width="240px"
+  >
+    <template v-slot:front>
+      <CardFace v-bind:type="type"/>
+    </template>
+    <template v-slot:back>
+      <CardBack v-bind:type="type"/>
+    </template>
+  </vue-flip>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import VueFlip from 'vue-flip';
+import CardBack from '@/components/CardBack.vue';
+import CardFace from '@/components/CardFace.vue';
+
+
+export default Vue.extend({
+  name: 'Card',
+  components: {
+    VueFlip,
+    CardBack,
+    CardFace,
+  },
+  props: {
+    type: {
+      default: 'question',
+      type: String,
+      required: true,
+    },
+    text: String,
+  },
+
+  data: () => ({
+  }),
+  computed: {
+    classObject() {
+      return {
+        card: true,
+        question: this.type === 'question',
+      };
+    },
+  },
+  methods: {
+  },
+});
+</script>
---
+++ src/components/CardFace.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container class='pa-0'>
+    <div class='cardContainer'>
+      <div v-bind:class="classObject">
+        <p class='text'>Cards Against Clones</p>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'CardFace',
+  props: {
+    type: {
+      default: 'question',
+      type: String,
+    },
+    text: String,
+  },
+
+  data: () => ({
+  }),
+  computed: {
+    classObject() {
+      return {
+        card: true,
+        question: this.type === 'question',
+      };
+    },
+  },
+  methods: {
+  },
+});
+</script>
+
+<style scoped>
+  .card{
+  /* Card Sizing */
+  width: 2.5in;
+  height: 4in;
+  border-radius: .25in;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  /* Display Properties */
+  float: left;
+  /* margin: 10px; */
+  }
+
+  .question{
+    border: 3px solid black;
+    color: white;
+    background: black;
+  }
+  .text{
+    /*font information*/
+    font-family: Helvetica,sans-serif;
+    font-weight: bolder;
+    font-size: 32px;
+    padding-top: 7%;
+    padding-left:7%;
+    padding-right:7%;
+    word-wrap: break-word;
+  }
+  .rounded-card{
+    border-radius:50px;
+  }
+
+</style>
